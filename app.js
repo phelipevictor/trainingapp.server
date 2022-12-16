3fc8f5c40b2c9d88efde7d77c81de7e72c16107a
Phelipe Victor <phelipevictor5@gmail.com>
2022-12-16 19:06:40 -0300

    test

--- app.js
+++ app.js
@@ -1,42 +1,38 @@
-const express = require('express');
-const connectDb = require('./config/db.config.js');
-const morgan = require('morgan')
-const cors = require('cors')
-require('dotenv').config();
+const express = require("express");
+const connectDb = require("./config/db.config.js");
+const morgan = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
 
-const trainingRoutes = require('./routes/training.routes.js')
-const exerciseRoutes = require('./routes/exercise.routes.js')
-const userRoutes = require('./routes/user.routes.js')
-const authRoutes = require('./routes/auth.routes.js')
+const trainingRoutes = require("./routes/training.routes.js");
+const exerciseRoutes = require("./routes/exercise.routes.js");
+const userRoutes = require("./routes/user.routes.js");
+const authRoutes = require("./routes/auth.routes.js");
 
-connectDb()
-const app = express()
-app.use(express.json())
-app.use(morgan('dev'))
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-})
-)
+connectDb();
+const app = express();
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
 
 // Rotas públicas
 
-app.use('/', authRoutes)
+app.use("/", authRoutes);
 
 // Middlewares
 
-app.use(require('./middlewares/auth.middleware'))
+app.use(require("./middlewares/auth.middleware"));
 
 // Rotas Privadas
 
-app.use('/', trainingRoutes)
-app.use('/', exerciseRoutes)
-app.use('/', userRoutes)
+app.use("/", trainingRoutes);
+app.use("/", exerciseRoutes);
+app.use("/", userRoutes);
 
 // Listen
 
-require('./error-handling')(app)
+require("./error-handling")(app);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server listen on Port ${process.env.PORT}`)
-})
+  console.log(`Server listen on Port ${process.env.PORT}`);
+});
